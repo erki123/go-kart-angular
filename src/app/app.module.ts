cfae18c9f27742d99cbd0af15a9eb9c2944a662d
Erki <alas.erki@gmail.com>
2024-07-06 14:02:57 +0300

    Updated nav bar and footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { RacesComponent } from './components/races/races.component';
 import { EShopComponent } from './components/e-shop/e-shop.component';
-
-const routes: Routes = [
-    {path: '', component: HomeComponent },
-    {path: 'login', component: LoginComponent },
-    {path: 'races', component: RacesComponent },
-    {path: 'e-shop', component: EShopComponent },
-];
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -34,7 +25,6 @@ const routes: Routes = [
     FormsModule,
     CommonModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/e-shop/e-shop.component.ts
+++ src/app/components/e-shop/e-shop.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-e-shop',
   templateUrl: './e-shop.component.html',
   styleUrls: ['./e-shop.component.css']
 })
-export class EShopComponent {
-  
+export class EShopComponent implements OnInit {
+  products = [
+    {
+      id: 1,
+      title: 'Product 1',
+      description: 'This is a short description of product 1.',
+      price: 19.99,
+      imageUrl: 'https://placehold.co/300x300/0000FF/808080?text=Product1'
+    },
+    {
+      id: 2,
+      title: 'Product 2',
+      description: 'This is a short description of product 2.',
+      price: 29.99,
+      imageUrl: 'https://placehold.co/300x300/0000FF/808080?text=Product2'
+    },
+
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 }
